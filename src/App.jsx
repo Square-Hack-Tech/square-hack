0cc862e717b364ae364a2a6000bebbd4b6f95df6
prakash7050 <prak5682@gmail.com>
2024-06-22 09:49:24 +0530

    feat: add project boilerplate, closes #1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <img src='/logo.svg' className='App-logo' alt='logo' height={100} />
+      <h1>Square Hack</h1>
+    </>
+  )
+}
+
+export default App

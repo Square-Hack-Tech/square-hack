c5d6c546e927be1aedb29ef7b7c7df551a45096b
priyanshuredu <priyanshured12@gmail.com>
2025-05-08 01:56:25 +0530

    feat : a new form added to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import ListSection from '../components/ListSection'
+import FormSection from '../components/FormSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <ListSection />
+      <FormSection/>
     </>
   )
 }
